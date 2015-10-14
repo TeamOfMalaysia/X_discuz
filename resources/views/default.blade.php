2e25c6e705f898a68d593adaa23ab5e2c2e36a8f
anni <lian3204321@gmail.com>
2015-10-14 09:05:32 +0800

    完成头部页面一半的开发

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -1,19 +1,26 @@
-<html>
+<!DOCTYPE html>
+
+<html lang="en">
+
 <head>
     <title>discuz_name</title>
     @section('css-main')
-    <link rel="stylesheet" src="">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
     @show
 </head>
+
 <body>
+<div class='container'>
     @include('forum.header')
 
     @yield('contents')
 
     @include('forum.footer')
-
+</div>
     @section('js-main')
-    <script type="application/javascript" src=""></script>
+        <script type="application/javascript" src="{{ asset('js/jquery.js') }}"></script>
+        <script type="application/javascript" src="{{ asset('js/bootstrap.min.js') }}"></script>
+
     @show
 </body>
 </html>
--- resources/views/forum/header.blade.php
+++ resources/views/forum/header.blade.php
@@ -1,1 +1,52 @@
-header
+<link rel="stylesheet" href="{{ asset('css/header.css') }}">
+
+<div class="row">
+    <ol class="breadcrumb" >
+        <li><a href="#">设为首页</a></li>
+        <li><a href="#">收藏本站</a></li>
+        <li><a href="#">切换宽度</a></li>
+    </ol>
+    <div class="col-lg-6">
+        <img src="{{ asset('image/common/logo.png') }}" alt="discuz"/>
+    </div>
+    <div class="col-lg-5">
+        <form method="post" action="" class="form-horizontal">
+                <div class="row">
+                    <div class="form-group col-lg-9">
+                        <label class="col-lg-4 control-label" for="inputName">
+                            <select >
+                                <option selected>用户名</option>
+                                <option>Email</option>
+                            </select>
+                        </label>
+                        <div class="col-lg-8">
+                            <input type="text" class="form-control" id="inputName"/>
+                        </div>
+                    </div>
+                    <div class="checkbox col-lg-3">
+                        <label>
+                            <input type="checkbox" />
+                            自动登陆
+                        </label>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="form-group col-lg-9">
+                        <label class="col-lg-4 control-label" for="inputPassword">
+                            密码
+                        </label>
+                        <div class="col-lg-8 ">
+                            <input type="password" class="form-control" id="inputPassword"/>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <button type="submit" class="btn btn-primary ">登陆</button>
+                    </div>
+                </div>
+        </form>
+
+    </div>
+    <div class="col-lg-1">
+
+    </div>
+</div>
